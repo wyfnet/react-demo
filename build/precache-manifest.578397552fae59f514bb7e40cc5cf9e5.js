bd31a236623ee4c223c2b0933e60c25c6abc4423
wuhai <485548341@qq.com>
2019-03-19 10:46:23 +0800

    add

---
+++ build/precache-manifest.578397552fae59f514bb7e40cc5cf9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b50bed57e2f911d2f0c",
+    "url": "/static/js/main.29ef6470.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "6b50bed57e2f911d2f0c",
+    "url": "/static/css/main.2a7b2f36.chunk.css"
+  },
+  {
+    "revision": "cfc26a8e8885d813ef973529bb60a5be",
+    "url": "/index.html"
+  }
+];
